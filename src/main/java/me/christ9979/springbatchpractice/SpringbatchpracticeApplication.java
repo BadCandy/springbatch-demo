f65101efa78bb974500ab22c90fafaf08f73ec08
christ9979 <christ9979@naver.com>
2019-07-09 11:13:27 +0900

    feat(Init): Init spring batch project.

---
+++ src/main/java/me/christ9979/springbatchpractice/SpringbatchpracticeApplication.java
@@ -0,0 +1,13 @@
+package me.christ9979.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchpracticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/me/christ9979/springbatchpractice/hello/Person.java
@@ -0,0 +1,14 @@
+package me.christ9979.springbatchpractice.hello;
+
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String lastName;
+    private String firstName;
+}
